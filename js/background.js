ce41c1aacfb5ac961028baed82f0c38f79390785
richagithub <richa09me@gmail.com>
2016-01-11 02:41:42 +0530

    resize images

--- js/background.js
+++ js/background.js
@@ -1,9 +1,8 @@
 function loading(){
 
-var imgarr=["images/img1.jpg","images/img2.jpg","images/img3.jpg","images/img4.jpg"
+var imgarr=["images/img1.jpg","images/img2.jpg","images/img3.JPG","images/img4.JPG"
             ,"images/img5.jpg","images/img6.jpg","images/img7.jpg","images/img8.jpg"
-			,"images/img9.jpg","images/img10.jpg","images/img11.jpg","images/img12.jpg"
-			,"images/img13.jpg","images/img14.jpg","images/img15.jpg"];
+			,"images/img9.jpg","images/img10.jpg","images/img11.jpg","images/img12.jpg"];
 var spot=Math.floor(Math.random()*imgarr.length);
 document.body.style.backgroundImage="url("+imgarr[spot]+")";
 /*  
